4c3d08be07ca5718dbf77facfd69d0fbc5b9000e
roshanatl <roshan.atl@gmail.com>
2015-07-28 16:48:59 +0530

    Comparable Interface

---
+++ niit/comparableInterface/ComparableMain.java
@@ -0,0 +1,18 @@
+package com.niit.comparableInterface;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ComparableMain {
+
+	public static void main(String[] args) {
+		  Set<Student> studentSet = new TreeSet<>();
+	        studentSet.add(new Student("Atul",101,129945));
+	        studentSet.add(new Student("Vtul",031,126545));
+	        studentSet.add(new Student("Mtul",401,12845));
+	        studentSet.add(new Student("Rtul",501,12745));
+	        studentSet.add(new Student("Utul",701,12645));
+	        System.out.println(studentSet);
+	}
+
+}
---
+++ niit/comparableInterface/Student.java
@@ -0,0 +1,24 @@
+package com.niit.comparableInterface;
+
+public class Student implements Comparable<Student>, Cloneable {
+	String Name;
+	int Id;
+	double Gpa;
+
+	@Override
+	public int compareTo(Student o) {
+		int result = 0;
+		result = this.Name.compareTo(o.Name);		
+		return result;
+	}
+
+	Student(String Name, int Id, double Gpa) {
+		this.Name = Name;
+		this.Id = Id;
+		this.Gpa = Gpa;
+	}
+
+	public String toString() {
+		return ( '\n' +" Name : " + Name + " Id = " + Id + " Gpa = " + Gpa );
+	}
+}
